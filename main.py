8b05df2a11d8fcd514c9b2fdcdc482778b264835
w1nfi <animewinfi@gmail.com>
2023-06-23 15:05:38 +0300

    add commands_dict and perpetual listening

--- main.py
+++ main.py
@@ -1,56 +1,52 @@
-import speech_recognition
-import time
+import random
+import speech_recognition as sr
+import sys
 
+sr_obj = sr.Recognizer()
+sr_obj.pause_threshold = 0.5
 
-sr = speech_recognition.Recognizer()
-sr.pause_threshold = 2
+commands_dict = {
+    'greeting': ['привет', 'приветствую', 'дарова'],
+    'create_task': ['добавить задачу', 'создать задачу', 'заметка'],
+    'exit': ['выход', 'закрыть', 'пока']
+}
 
-def hello():
-    return "Привет, чем могу помочь?"
-
-def note():
-    return "Что вы хотите записать?"
-
-
-def create_note():
-    def get_voice_input():
-        with speech_recognition.Microphone() as mic:
-            sr.adjust_for_ambient_noise(source=mic, duration=0.5)
-            audio = sr.listen(source=mic)
-            try:
-                query = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
-                return query
-            except speech_recognition.UnknownValueError:
-                print("Не удалось распознать речь.")
-    
-    with speech_recognition.Microphone() as mic:
-        sr.adjust_for_ambient_noise(source=mic, duration=0.5)
-        print(note())
-        audio = sr.listen(source=mic, timeout=2.0)  # Установили таймаут записи в 2 секунды
-        try:
-            query = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
-            
-            with open('note-list.txt', 'a', encoding='utf-8') as file:
-                file.write(f'{query}\n')
-                print("Задача записана, хотите просмотреть, что записали? да/нет")
-                response = get_voice_input()
-                if response == "да":
-                    print(query)
-                else:
-                    print("Удачи")
-        except speech_recognition.UnknownValueError:
-            print("Не удалось распознать речь.")
-        except speech_recognition.WaitTimeoutError:
-            print("Прошло две секунды тишины. Запись завершена.")
-
-with speech_recognition.Microphone() as mic:
-    sr.adjust_for_ambient_noise(source=mic, duration=0.5)
-    audio = sr.listen(source=mic)
+def listen_command():
     try:
-        query = sr.recognize_google(audio_data=audio, language='ru-RU').lower()
-        if query in ["дарова", "привет", "приветики", "ку", "здарова", "салам"]:
-            print(hello())
-        elif query in ["открой заметки", "заметки", "блокнот", "запись"]:
-            create_note()
-    except speech_recognition.UnknownValueError:
-        print("Не удалось распознать речь.")
+        with sr.Microphone() as source:
+            sr_obj.adjust_for_ambient_noise(source=source, duration=0.5)
+            audio = sr_obj.listen(source=source)
+            query = sr_obj.recognize_google(audio, language='ru-RU').lower()
+
+        return query
+    except sr.UnknownValueError:
+        return 'Damn... Не понял что ты сказал :/'
+
+def greeting():
+    return 'Привет, чем могу помочь!'
+
+def create_task():
+    print('Что добавим в список дел?')
+    query = listen_command()
+
+    with open('todo-list.txt', 'a', encoding='utf-8') as file:
+        file.write(f'❗️ {query}\n')
+
+    return f'Задача {query} добавлена в todo-list!'
+
+def exit_program():
+    print('До свидания!')
+    sys.exit()
+
+def main():
+    query = listen_command()
+
+    for command, keywords in commands_dict.items():
+        if query in keywords:
+            if command == 'exit':
+                exit_program()
+            else:
+                print(globals()[command]())
+
+if __name__ == '__main__':
+    main()
