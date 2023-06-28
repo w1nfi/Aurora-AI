d08920e0c723dad6c04b3fe95a8470451eca97f9
w1nfi <animewinfi@gmail.com>
2023-06-28 21:13:34 +0300

    added two functions that are not working yet

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import speech_recognition as sr
 import sys
 import subprocess
 
+import pyautogui
+
+
 sr_obj = sr.Recognizer()
 sr_obj.pause_threshold = 0.5
 
@@ -10,7 +13,10 @@ commands_dict = {
     'greeting': ['привет', 'приветствую', 'дарова'],
     'create_task': ['добавить задачу', 'создать задачу', 'заметка'],
     'open_browser': ['открой браузер', 'открыть браузер'],
-    'exit': ['выход', 'закрыть', 'пока']
+    'open_google': ['открой гугл', 'открыть гугл'],
+    'exit': ['выход', 'закрыть', 'пока'],
+    'hide_windows': ['скрой окна', 'скрой все окна', 'скрой все'],
+    'play_phonk': ['включи фонк', 'воспроизведи фонк','вруби фонк'],
 }
 
 def listen_command():
@@ -22,7 +28,7 @@ def listen_command():
 
         return query
     except sr.UnknownValueError:
-        return 'Damn... Не понял что ты сказал :/'
+        return 'Вытяни хуй изо рта и потом скажи'
 
 def greeting():
     return 'Привет, чем могу помочь!'
@@ -36,8 +42,18 @@ def create_task():
 
     return f'Задача {query} добавлена в todo-list!'
 
+def open_google():
+    subprocess.Popen(r'C:\Program Files\Google\Chrome\Application\chrome.exe')
+
 def open_browser():
     subprocess.Popen(r'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
+
+def hide_windows():
+    pyautogui.hotkey('win', 'd')
+
+def play_phonk():
+    music_directory = r'C:\Users\lomba\Desktop\Spotify\SpotiFlyer\Playlists\God_'
+    subprocess.Popen(r'explorer.exe ' + music_directory)
 
 def exit_program():
     print('До свидания!')
@@ -48,12 +64,14 @@ def main():
 
     for command, keywords in commands_dict.items():
         if query in keywords:
+            print(f"Recognized command: {command}")
             if command == 'exit':
                 exit_program()
-            elif command == 'open_browser':
-                open_browser()
             else:
+                print(f"Executing command: {command}")
                 print(globals()[command]())
+
+
 
 if __name__ == '__main__':
     main()
